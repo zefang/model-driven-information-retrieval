4aea9d0f78395a479dce3c9c8c8fc20d588b4c1e
stefano.celentano87 <stefano.celentano87@gmail.com>
2011-05-01 11:32:17 +0000

    Aggiunta DataSourceConnectionConfigPluginImpl per il model crawler

---
+++ it.polimi.mdir.modelcrawler/code/src/it/polimi/mdir/crawler/model/DataSourceConnectionConfigPluginImpl.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Contributors: Stefano Celentano, Lorenzo Furrer
+ **********************************************************************************************************************/
+
+package it.polimi.mdir.crawler.model;
+
+import org.eclipse.smila.connectivity.framework.schema.DataSourceConnectionConfigPlugin;
+
+/**
+ * The Class DataSourceConnectionConfigPluginImpl.
+ */
+public class DataSourceConnectionConfigPluginImpl implements DataSourceConnectionConfigPlugin {
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see org.eclipse.smila.connectivity.framework.schema.DataSourceConnectionConfigPlugin#getSchemaLocation()
+   */
+  @Override
+  public String getSchemaLocation() {
+    return "schemas/FileSystemDataSourceConnectionConfigSchema.xsd";
+  }
+
+  @Override
+  public String getMessagesPackage() {
+	// TODO Auto-generated method stub
+	return null;
+  }
+  
+}
+
