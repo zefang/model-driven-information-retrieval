6920b2fa71d4c3eab2c1a3a9cdb0e0887fd82cf5
Lorenzo.Furrer <Lorenzo.Furrer@dd02c6bc-fb39-5e23-e1a6-f891a138d0db>
2011-05-07 19:22:44 +0000

    Added it.polimi.mdir.solr.plugin containing classes to manage the Payload and the query parsers.

---
+++ it.polimi.mdir.solr.plugin/code/src/it/polimi/mdir/solr/plugin/MyPerFieldSimilarityWrapper.java
@@ -0,0 +1,72 @@
+package it.polimi.mdir.solr.plugin;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.lucene.search.DefaultSimilarity;
+import org.apache.lucene.search.Similarity;
+
+/**
+ * A delegating Similarity implementation similar to PerFieldAnalyzerWrapper.
+ */
+public class MyPerFieldSimilarityWrapper extends Similarity {
+
+  private static final long serialVersionUID = -7777069917322737611L;
+
+  private Similarity defaultSimilarity;
+  private Map<String,Similarity> fieldSimilarityMap; 
+  
+  public MyPerFieldSimilarityWrapper() {
+    this.defaultSimilarity = new DefaultSimilarity();
+    this.fieldSimilarityMap = new HashMap<String,Similarity>();
+    this.fieldSimilarityMap.put("text_C", new PayloadSimilarity());
+  }
+  
+  @Override
+  public float coord(int overlap, int maxOverlap) {
+    return defaultSimilarity.coord(overlap, maxOverlap);
+  }
+
+  @Override
+  public float idf(int docFreq, int numDocs) {
+    return defaultSimilarity.idf(docFreq, numDocs);
+  }
+
+  @Override
+  public float lengthNorm(String fieldName, int numTokens) {
+    Similarity sim = fieldSimilarityMap.get(fieldName);
+    if (sim == null) {
+      return defaultSimilarity.lengthNorm(fieldName, numTokens);
+    } else {
+      return sim.lengthNorm(fieldName, numTokens);
+    }
+  }
+
+  @Override
+  public float queryNorm(float sumOfSquaredWeights) {
+    return defaultSimilarity.queryNorm(sumOfSquaredWeights);
+  }
+
+  @Override
+  public float sloppyFreq(int distance) {
+    return defaultSimilarity.sloppyFreq(distance);
+  }
+
+  @Override
+  public float tf(float freq) {
+    return defaultSimilarity.tf(freq);
+  }
+  
+  @Override
+  public float scorePayload(int docId, String fieldName,
+      int start, int end, byte[] payload, int offset, int length) {
+    Similarity sim = fieldSimilarityMap.get(fieldName);
+    if (sim == null) {
+      return defaultSimilarity.scorePayload(docId, fieldName, 
+        start, end, payload, offset, length);
+    } else {
+      return sim.scorePayload(docId, fieldName, 
+        start, end, payload, offset, length);
+    }
+  }
+}
