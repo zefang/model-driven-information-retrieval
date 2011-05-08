d9c743211b0cd87e2b18eff0c819d047becec848
stefano.celentano87@gmail.com <stefano.celentano87@gmail.com@dd02c6bc-fb39-5e23-e1a6-f891a138d0db>
2011-05-08 15:01:05 +0000

    added EnrichRecordsExperimentAPipelet

---
+++ it.polimi.mdir.text/code/src/it/polimi/mdir/text/EnrichRecordsExperimentAPipelet.java
@@ -0,0 +1,87 @@
+package it.polimi.mdir.text;
+
+
+import java.util.ArrayList;
+
+import org.eclipse.smila.blackboard.Blackboard;
+import org.eclipse.smila.blackboard.BlackboardAccessException;
+import org.eclipse.smila.datamodel.AnyMap;
+import org.eclipse.smila.datamodel.Record;
+import org.eclipse.smila.datamodel.filter.RecordFilterNotFoundException;
+import org.eclipse.smila.processing.Pipelet;
+import org.eclipse.smila.processing.ProcessingException;
+
+
+public class EnrichRecordsExperimentAPipelet implements Pipelet {
+		
+	@Override
+	public void configure(AnyMap configuration) throws ProcessingException {
+		
+	}
+
+	@Override
+	public String[] process(Blackboard blackboard, String[] recordIds)
+			throws ProcessingException {
+		
+		//This array will contain new id to pass through the framework
+		ArrayList<String> newRecordsIds = new ArrayList<String>();
+		int nNewRecords;
+		String content = "";
+		String attr = "";
+		String attributeNames = "";
+		for (final String id : recordIds) {
+			try {
+				
+				//Extract attributes from record
+				String classNames = blackboard.getRecord(id).getMetadata().getStringValue("classNames");
+				String attributeNamesAndClassIds = blackboard.getRecord(id).getMetadata().getStringValue("attributeNames");
+				String projectId = blackboard.getRecord(id).getMetadata().getStringValue("projectId");
+				
+				//Split attributeNames into an string array
+				String[] attributeNamesArray = attributeNamesAndClassIds.split("\\s");
+				
+				//Split attributeNamesArray into "classId attribute" to extract only attribute names
+				for (int i=0; i<attributeNamesArray.length; i++) {
+					
+					attr = attributeNamesArray[i].split("\\$")[1];
+					
+					if (i==0) {			
+						attributeNames = attributeNames.concat(attr);
+					}
+					else {		
+						attributeNames = attributeNames.concat(" " + attr);					
+					}	
+				}
+				
+				content = attributeNames.concat(" " + classNames);
+				
+				Record copy = blackboard.copyRecord(id, projectId);
+					
+				//Adding new id to newRecordsIds
+				newRecordsIds.add(projectId);
+
+				
+				copy.getMetadata().put("content", content);
+
+				//Apply record filter
+				try {
+					copy = blackboard.filterRecord(copy, "experimentA");
+				} catch (RecordFilterNotFoundException e) {
+					e.printStackTrace();
+				}		
+				blackboard.setRecord(copy);				
+				blackboard.commit();	
+				
+			} catch (BlackboardAccessException e) {
+				System.out.println("BlackboardAccessException: " + e.toString());
+				e.printStackTrace();
+			}
+		}
+		
+		nNewRecords = newRecordsIds.size();
+		String[] newRecordsIdsArray = new String[nNewRecords];
+		newRecordsIds.toArray(newRecordsIdsArray);
+		return newRecordsIdsArray;
+	}
+
+}
