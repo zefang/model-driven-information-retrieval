893cd24edab30eb0cc72eb875bbcb44f95a89775
stefano.celentano87 <stefano.celentano87@gmail.com>
2011-05-16 09:05:49 +0000

    Updated TestServle. Now it writes XML query output to file keeping original indentation.

--- SolrInstance/src/it/polimi/mdir/solr/test/TestServlet.java
+++ SolrInstance/src/it/polimi/mdir/solr/test/TestServlet.java
@@ -3,6 +3,10 @@ package it.polimi.mdir.solr.test;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
+import javax.xml.transform.*;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+
 import java.io.*;
 import java.net.*;
 
@@ -15,15 +19,21 @@ public class TestServlet extends HttpServlet {
 							 "http://localhost:8983/solr/text_experiment_B/select?qf=content&fl=*%2Cscore&qt=PLDisMaxQParserPlugin&start=0&rows=10&wt=standard&debugQuery=on", 
 							 "http://localhost:8983/solr/text_experiment_C/select?qf=content&fl=*%2Cscore&qt=PLDisMaxQParserPlugin&start=0&rows=10&wt=standard&debugQuery=on"};
 	
-	String query = "";
-	String mm = "";
+	private String query = "";
+	private String mm = "";
+	
+	// This string will contain the lines coming from the servlet which is called
+	String line = "";
+	// This string will contain the response from the servlet which is called
+	String response = "";
 		
 	  public void init () throws ServletException {
 	  }
 	
 	  public void doGet (HttpServletRequest req, HttpServletResponse res)
 	    throws ServletException, IOException {
-	
+		  
+		  // Retrieving parameters from the jsp page input form
 		  query = req.getParameter("q");
 		  mm = req.getParameter("mm");
 		  
@@ -31,10 +41,7 @@ public class TestServlet extends HttpServlet {
 		  for (int i=0; i<urls.length; i++) {
 			  urls[i] = urls[i].concat("&q=" + query);
 			  urls[i] = urls[i].concat("&mm=" + mm);		  
-		  }
-	      
-		  // This string will contain the response from the servlet which is called
-		  String response = "";
+		  }	    
 		  
 		  for (int i=0; i<experiments.length; i++) {
 			  // New file		  
@@ -50,9 +57,15 @@ public class TestServlet extends HttpServlet {
 			  // Read in the servlet response
 		      BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));      
 			 
-		      // Write to file
-		      while ((response = in.readLine()) != null)
-		    	  	file.write(response + '\n');
+		      // Cleaning response
+		      response = "";
+		      
+		      // Fetch lines from input
+		      while ((line = in.readLine()) != null)
+		    	  response += line;
+		    
+		      // Writing to file keeping indentation
+		      file.write(writeXML(response));
 		      
 		      file.close();
 		    	  
@@ -66,6 +79,27 @@ public class TestServlet extends HttpServlet {
 	
 	  public void destroy () {
 	  }
+	  
+	  // Write XML to file keeping indentation
+	  public static String writeXML(String input, int indent) {
+		    try {
+		        Source xmlInput = new StreamSource(new StringReader(input));
+		        StringWriter stringWriter = new StringWriter();
+		        StreamResult xmlOutput = new StreamResult(stringWriter);
+		        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+		        transformerFactory.setAttribute("indent-number", indent);
+		        javax.xml.transform.Transformer transformer = transformerFactory.newTransformer(); 
+		        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+		        transformer.transform(xmlInput, xmlOutput);
+		        return xmlOutput.getWriter().toString();
+		    } catch (Exception e) {
+		        throw new RuntimeException(e);
+		    }
+		}
+
+		public static String writeXML(String input) {
+		    return writeXML(input, 2);
+		}
 	
 	  public String getServletInfo () {
 	    return "....";
