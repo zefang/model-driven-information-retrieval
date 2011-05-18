e214138dc103efefdff222ab1c9a87957347ddbd
Lorenzo.Furrer <Lorenzo.Furrer@dd02c6bc-fb39-5e23-e1a6-f891a138d0db>
2011-05-18 16:24:17 +0000

    Added stub of the pseudocode for the importing of attributes. Plus a couple of issues

--- it.polimi.mdir.graph/code/src/it/polimi/mdir/graph/ImportAttributes.java
+++ it.polimi.mdir.graph/code/src/it/polimi/mdir/graph/ImportAttributes.java
@@ -4,8 +4,27 @@ public class ImportAttributes extends OperationFunction {
 
 	@Override
 	public void importAttributes(String from, String rootNode) {
+		
 		System.out.println("I'm importing the attributes from "+ from +" to: " + rootNode);
 		
+		if (from.equals(rootNode)) {
+			//Sono nel nodo originale, 
+			//importo gli attributi vanilla
+		} else {
+			//1)retrieve vanilla attributes from "from"
+			
+			//2)importo attributi "importati" di "from"
+			// guardare nell'edge che ha souce=root e target=from
+			// a seconda del tipo dare una penalità diversa agi attributi che importo
+			
+			//TODO attenzione! quello qui sopra non va bene!
+			// magari from e root non sono connessi!!! 
+			//										quindi rootNode non serve?
+			//										usare una  variabile che si "ingrossa"?
+			//
+			// TODO attenzione!!! bisogna prima risolverre i cicli! o forse no? 
+			// se non lo facciamo è sbagliatissimo o cambia poco?
+		}
 	}
 	
 }
