117633539a467e7fc7a54d0c328fedc5435c832d
Lorenzo.Furrer <Lorenzo.Furrer@dd02c6bc-fb39-5e23-e1a6-f891a138d0db>
2011-05-18 10:47:58 +0000

    Restructured NavigateGraph to use recursion.

--- it.polimi.mdir.graph/code/src/it/polimi/mdir/graph/NavigateGraph.java
+++ it.polimi.mdir.graph/code/src/it/polimi/mdir/graph/NavigateGraph.java
@@ -14,40 +14,87 @@ public class NavigateGraph {
 	
 	private static final int MAX_HOPS = 1;
 	
-	public static void main(String[] args) {
-		
-		//Get all nodes
-		ArrayList<String> nodeList = new ArrayList<String>();
-		XQueryWrapper xq = new XQueryWrapper(XQUERY_PATH + "/getNodeIds.xquery");
-		xq.bindVariable("document", FILE_PATH + "result.xml"); //TODO ciclare
-		nodeList = xq.executeQuery();
-		
-		// for each node get its neighbours
-		for (String nodeId : nodeList)  {
-			//adding in the queue
-			LinkedList<String> neighboursQueue = new LinkedList<String>();
-			ArrayList<String> neighboursList = getNeighbours(nodeId);
-			Iterator<String> itr = neighboursList.iterator();
-			while (itr.hasNext()) {
-				neighboursQueue.add(itr.next());
-			}
+	private LinkedList<String> _nodeQueue = new LinkedList<String>();
 
-			//TODO visit the node
-			
-			//visit the neighbours
-			for (String neighbourId : neighboursQueue) {
-				//TODO import attributes in the starting node
-			}
-			
-			
+	public void startNavigation() {
+		
+		int residualHops = MAX_HOPS;
+		_nodeQueue = getAllNodes();
+		
+		while (!_nodeQueue.isEmpty()) {
+			visitNode(_nodeQueue.remove(), --residualHops); 
+			//TODO aggiungere parametro per dire a quale nodo fare riferimento 
+			// (eg, a quale nodo passare gli attributi raccolti)
 		}
 	}
 	
-	private static ArrayList<String> getNeighbours(String nodeId) {
+	/**
+	 * 	Visits recursively the node and its neighbours. 
+	 *  
+	 * @param nodeId
+	 * the id of the node to visit.
+	 * @param residualHops
+	 * Tells us how much hops are left before stopping. 
+	 * It's how deep we are in the visiting.
+	 * 
+	 */
+	private void visitNode(String nodeId, int residualHops) {
+		if (residualHops == 0) 
+			return;
+		
+		residualHops -= 1;
+		LinkedList<String> neighboursQueue = getNeighbours(nodeId);
+		
+		//TODO visit this node
+		
+		//visit the neighbours
+		while (!neighboursQueue.isEmpty()) {
+			visitNode(neighboursQueue.remove(), residualHops);
+		}
+	}
+	
+	/**
+	 * Gets All the nodes. 
+	 * The associated Xquery retrieves all the id attributes of the tag "node" 
+	 * 
+	 * @return
+	 * A queue containing all the nodes ids.
+	 */
+	private LinkedList<String> getAllNodes() {
+		XQueryWrapper xq = new XQueryWrapper(XQUERY_PATH + "/getNodeIds.xquery");
+		xq.bindVariable("document", FILE_PATH + "result.xml"); //TODO ciclare
+		ArrayList<String> nodesList = xq.executeQuery();
+		LinkedList<String> nodesQueue = new LinkedList<String>();
+		Iterator<String> itr = nodesList.iterator();
+		while (itr.hasNext()) {
+			nodesQueue.add(itr.next());
+		}
+		return nodesQueue;
+	}
+	
+	
+	/**
+	 * 
+	 * Gets the neighbours of a node. 
+	 * The associated Xquery retrieves all the target attributes of edges
+	 * that have the provided id as source attribute. 
+	 * 
+	 * @param nodeId
+	 * The id of the node.
+	 * @return
+	 * A queue of the neighbours of the node.
+	 */
+	private static LinkedList<String> getNeighbours(String nodeId) {
 		XQueryWrapper xq = new XQueryWrapper(XQUERY_PATH + "/getNeighbours.xquery");
 		xq.bindVariable("document", FILE_PATH + "result.xml"); //TODO ciclare
 		xq.bindVariable("nodeId", nodeId);
-		return xq.executeQuery();
+		ArrayList<String> neighboursList = xq.executeQuery();
+		LinkedList<String> neighboursQueue = new LinkedList<String>();
+		Iterator<String> itr = neighboursList.iterator();
+		while (itr.hasNext()) {
+			neighboursQueue.add(itr.next());
+		}
+		return neighboursQueue;
 	}
 
 }
