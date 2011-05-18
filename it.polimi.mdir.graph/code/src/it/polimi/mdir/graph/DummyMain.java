04e1efc05182ac78e33499a7fd8507969021d9bb
Lorenzo.Furrer <Lorenzo.Furrer@dd02c6bc-fb39-5e23-e1a6-f891a138d0db>
2011-05-18 15:03:43 +0000

    - Modified NavigateGraph to be able to call visitNode - Added OperationFunction and its implementation importAttributes

---
+++ it.polimi.mdir.graph/code/src/it/polimi/mdir/graph/DummyMain.java
@@ -0,0 +1,9 @@
+package it.polimi.mdir.graph;
+
+public class DummyMain {
+
+	public static void main(String[] args) {
+		(new NavigateGraph()).startNavigation();
+	}
+
+}
---
+++ it.polimi.mdir.graph/code/src/it/polimi/mdir/graph/OperationFunction.java
@@ -0,0 +1,10 @@
+package it.polimi.mdir.graph;
+
+public abstract class OperationFunction {
+
+	
+	abstract public void importAttributes(String from, String rootNode);
+	
+}
+
+
