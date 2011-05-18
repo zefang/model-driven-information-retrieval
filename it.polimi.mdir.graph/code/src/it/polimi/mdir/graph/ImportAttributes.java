8217f0060de5d7b51ca432c450d9c4bef1ae61fd
Lorenzo.Furrer <Lorenzo.Furrer@dd02c6bc-fb39-5e23-e1a6-f891a138d0db>
2011-05-18 18:40:51 +0000

    - clarified a bit the pseudocode

--- it.polimi.mdir.graph/code/src/it/polimi/mdir/graph/ImportAttributes.java
+++ it.polimi.mdir.graph/code/src/it/polimi/mdir/graph/ImportAttributes.java
@@ -15,10 +15,15 @@ public class ImportAttributes extends OperationFunction {
 		//la stringa attributes viene riempita mano a mano dagli attributi da importare
 		
 		//1) importo attributi vanilla da "from". 
+		//pesarli in base al numero di hop
+		//if numHop==0 -> no penalty 
+		//else multiply all normal weights by the penalty given by the type of relationship
+		//(same for following attributes)
 		
 		//2)importo attributi "importati" di "from"
-		// guardare nell'edge che ha souce=from
-		// a seconda del tipo dare una penalità diversa agi attributi che importo
+		// guardare nell'edge che ha source=from
+		// a seconda del tipo dare una penalità diversa agli attributi che importo
+		//pesarli in base al numero di hop
 		
 		// TODO attenzione!!! bisogna prima risolverre i cicli! o forse no? 
 		// se non lo facciamo è sbagliatissimo o cambia poco?
