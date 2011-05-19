57933c7381cb1a9daf8726bd6e70779b7fd935b4
Lorenzo.Furrer <Lorenzo.Furrer@dd02c6bc-fb39-5e23-e1a6-f891a138d0db>
2011-05-19 11:35:23 +0000

    Beginning to implement importing attributes logic. For now it imports all attributes as vanilla attributes with no weights.

--- it.polimi.mdir.graph/code/src/it/polimi/mdir/graph/ImportAttributes.java
+++ it.polimi.mdir.graph/code/src/it/polimi/mdir/graph/ImportAttributes.java
@@ -1,5 +1,8 @@
 package it.polimi.mdir.graph;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 import it.polimi.mdir.xquery.XQueryWrapper;
 
 
@@ -8,31 +11,42 @@ public class ImportAttributes extends OperationFunction {
 	public static String attributes = "";
 	
 	@Override
-	public void importAttributes(String currentNode, String callerNode) {
+	public void importAttributes(String currentNode, String callerNode, int numHops) {
 		
+		//get currentNode Name
 		XQueryWrapper xq = new XQueryWrapper("C:/Users/Lox/workspaceSMILA/it.polimi.mdir.graph/xquerytry/getClassName.xquery");
 		xq.bindVariable("document", "C:/Users/Lox/workspaceSMILA/it.polimi.mdir.graph/result/PetriNet_extended.uml.xml");
 		xq.bindVariable("id", currentNode);
-		System.out.println(currentNode);
 		String className = xq.executeQuery().get(0);
 		
+		//get callernode Name
 		XQueryWrapper xq2 = new XQueryWrapper("C:/Users/Lox/workspaceSMILA/it.polimi.mdir.graph/xquerytry/getClassName.xquery");
 		xq2.bindVariable("document", "C:/Users/Lox/workspaceSMILA/it.polimi.mdir.graph/result/PetriNet_extended.uml.xml");
 		xq2.bindVariable("id", callerNode);
 		String callerName = xq2.executeQuery().get(0);
 		System.out.println("This is " + className + " called from: " + callerName);
-		attributes += "!";
-		System.out.println(attributes);
 		
-		//la stringa attributes viene riempita mano a mano dagli attributi da importare
+		
 		
 		//1) importo attributi vanilla da "currentNode". 
 		//pesarli in base al numero di hop
-		//if numHop==0 -> no penalty 
-		//else multiply all normal weights by the penalty given by the type of relationship
+		//if numHop==1 -> no penalty perchè sono nel nodo di partenza
+		//else multiply all normal weights by the penalty given by the type of relationship between currentNode and callerNode
 		// To get the type of relationship check for the relationship that has
 		// 	source = callerNode and target = currentNode
-		//(same for following attributes)
+		//(same for attributes at number 2) )
+		XQueryWrapper xq3 = new XQueryWrapper("C:/Users/Lox/workspaceSMILA/it.polimi.mdir.graph/xquerytry/getVanillaAttributes.xquery");
+		xq3.bindVariable("document", "C:/Users/Lox/workspaceSMILA/it.polimi.mdir.graph/result/PetriNet_extended.uml.xml");
+		xq3.bindVariable("id", currentNode);
+		ArrayList<String> vanillaAttributes = xq3.executeQuery();
+		Iterator<String> vanillaAttributesIterator = vanillaAttributes.iterator();
+		while (vanillaAttributesIterator.hasNext()) {
+			attributes += vanillaAttributesIterator.next() + " "; //TODO mettere pesi
+		}
+		
+		if (numHops == 1) {
+			System.out.println(attributes);
+		}
 		
 		//2)importo attributi "importati" di "currentNode"
 		// guardare nell'edge che ha source=from
