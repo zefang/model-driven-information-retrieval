7c35ec47599d9651b3789ea506aff34960cf9905
Lorenzo.Furrer <Lorenzo.Furrer@dd02c6bc-fb39-5e23-e1a6-f891a138d0db>
2011-05-19 12:19:02 +0000

    Added WeightRules static class containing HashMaps for weights and penalties

---
+++ it.polimi.mdir.graph/code/src/it/polimi/mdir/graph/WeightRules.java
@@ -0,0 +1,29 @@
+package it.polimi.mdir.graph;
+
+import it.polimi.mdir.graph.TranslateXMItoGraphML.RelationType;
+
+import java.util.HashMap;
+
+public class WeightRules {
+
+	public final static HashMap<String, Float> weightMap = new HashMap<String, Float>();
+	static
+	{
+		//weights for imported attributes
+		weightMap.put("attribute", 1.0f); //vanilla attribute
+		weightMap.put("composition", 2.0f);
+		weightMap.put("association", 2.5f);
+	}
+	
+	public final static HashMap<RelationType, Float> penaltyMap = new HashMap<RelationType, Float>();
+	static
+	{
+		//penalties for relation types during navigation
+		penaltyMap.put(RelationType.COMPOSITION_COMPOSITE_COMPONENT, 0.5f);
+		penaltyMap.put(RelationType.COMPOSITION_COMPONENT_COMPOSITE, 0.6f);
+		penaltyMap.put(RelationType.ASSOCIATION, 0.5f);
+		penaltyMap.put(RelationType.GENERALIZATION_FATHER_CHILD, 1.0f);
+		penaltyMap.put(RelationType.GENERALIZATION_CHILD_FATHER, 0.75f);
+	}
+	
+}
