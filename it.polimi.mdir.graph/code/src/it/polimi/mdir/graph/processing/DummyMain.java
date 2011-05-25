594d64887d50e55bbeaadb681009daec0316548e
Lorenzo.Furrer <Lorenzo.Furrer@dd02c6bc-fb39-5e23-e1a6-f891a138d0db>
2011-05-25 19:03:32 +0000

    - New configuration.properties for the graph bundle. (found in dropbox) - Updated DummyMain to use serialization. (this is a mock-up for what will happen to the main program).

--- it.polimi.mdir.graph/code/src/it/polimi/mdir/graph/processing/DummyMain.java
+++ it.polimi.mdir.graph/code/src/it/polimi/mdir/graph/processing/DummyMain.java
@@ -1,18 +1,42 @@
 package it.polimi.mdir.graph.processing;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.Collection;
 import java.util.Iterator;
+
+import javax.xml.crypto.dsig.keyinfo.RetrievalMethod;
 
 import it.polimi.mdir.graph.Edge;
 import it.polimi.mdir.graph.Node;
 import edu.uci.ics.jung.graph.Graph;
 
 public class DummyMain {
-
+	
+	private static String fileName = "prova.uml.xml";
+	
 	public static void main(String[] args) {
+		
 		NavigateGraph nv = new NavigateGraph();
 		
-		Graph<Node, Edge> g = GraphFactory.createGraphFromGraphML(ConfigLoader.GRAPHML_PATH +"prova.uml.xml");
+		Graph<Node, Edge> g = GraphFactory.createGraphFromGraphML(ConfigLoader.GRAPHML_PATH + fileName);
+		//serializinf graph
+		try {
+			FileOutputStream fileOut = new FileOutputStream(ConfigLoader.SERIALIZATION_PATH + fileName+".ser");
+			ObjectOutputStream out = new ObjectOutputStream(fileOut);
+			out.writeObject(g);
+			out.close();
+				fileOut.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		//deserialize
+		g = retrieve();
 		Collection<Node> nodes = g.getVertices();
 		Iterator<Node> nodeItr = nodes.iterator();
 		Node toVisit = null;
@@ -23,8 +47,40 @@ public class DummyMain {
 				break;
 			}
 		}
-
 		nv.visitNode(g, 2, toVisit, new ProvaOperation());
+		
+		g = retrieve();
+		Collection<Node> nodes2 = g.getVertices();
+		Iterator<Node> nodeItr2 = nodes2.iterator();
+		while (nodeItr2.hasNext()) {
+			Node nextNode = nodeItr2.next();
+			if (nextNode.getClassName().equals("B")) {
+				toVisit = nextNode;
+				break;
+			}
+		}
+		nv.visitNode(g, 2, toVisit, new ProvaOperation());
+	}
+	
+	/**
+	 * Return a graph through deserialization
+	 */
+	@SuppressWarnings("unchecked")
+	private static Graph<Node, Edge> retrieve() {
+		Graph<Node, Edge> graph = null;
+		try {
+			FileInputStream fileIn = new FileInputStream(ConfigLoader.SERIALIZATION_PATH +fileName + ".ser");
+			ObjectInputStream in = new ObjectInputStream(fileIn);
+			graph = (Graph<Node, Edge>) in.readObject();
+			in.close();
+				fileIn.close();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return graph;
 	}
 	
 	
