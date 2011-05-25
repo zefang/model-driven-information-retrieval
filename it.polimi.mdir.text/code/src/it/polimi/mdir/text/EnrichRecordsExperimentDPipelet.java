26ddf6b75d8c361f2458f184a9fbca2a5c82d589
Lorenzo.Furrer <Lorenzo.Furrer@dd02c6bc-fb39-5e23-e1a6-f891a138d0db>
2011-05-25 14:15:55 +0000

    Now the weights of attributes with the same name gets averaged, so that a single instance of an attribute appears in the index.

--- it.polimi.mdir.text/code/src/it/polimi/mdir/text/EnrichRecordsExperimentDPipelet.java
+++ it.polimi.mdir.text/code/src/it/polimi/mdir/text/EnrichRecordsExperimentDPipelet.java
@@ -2,6 +2,7 @@ package it.polimi.mdir.text;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Iterator;
 
 import it.polimi.mdir.graph.Edge;
@@ -59,15 +60,11 @@ public class EnrichRecordsExperimentDPipelet implements Pipelet {
 				nv.visitNode(g, MAX_HOPS, toVisit, function);
 				ArrayList<String> attributes = function.getImportedAttributes();
 				ArrayList<String> classes = function.getImportedClassNames();
+				HashMap<String, SumCount> hm = new HashMap<String, SumCount>();
 				String newAttributes = "";
-				Iterator<String> itr = attributes.iterator();
-				while (itr.hasNext()) {
-					newAttributes += itr.next() + " ";
-				}
-				itr = classes.iterator();
-				while (itr.hasNext()) {
-					newAttributes += itr.next() + " ";
-				}
+				newAttributes += constructHashMapAndNewAttributes(attributes, hm);
+				newAttributes += constructHashMapAndNewAttributes(classes, hm);
+				newAttributes = averageWeight(newAttributes.trim(), hm);
 				
 				//copia tutto, anche gli importati,  nel campo attributeNames
 				blackboard.getRecord(id).getMetadata().put("attributeNames", newAttributes.trim());
@@ -86,4 +83,74 @@ public class EnrichRecordsExperimentDPipelet implements Pipelet {
 		return recordIds;
 	}
 
+	/**
+	 * Prepares the valuesArray to be used by the averageWeight() function.
+	 * @param valuesArray
+	 * The ArrayList containing the values in the form name|weight.
+	 * @param hm
+	 * The HashMap to store the couples Name-count-sum.
+	 * @return
+	 * The string of the names of the values separated by a space.
+	 */
+	private String constructHashMapAndNewAttributes(ArrayList<String> valuesArray, HashMap<String, SumCount> hm) {
+		String compositeValues = "";
+		String[] nextValue = new String[2];
+		Iterator<String> itr = valuesArray.iterator();
+		while (itr.hasNext()) {
+			nextValue = itr.next().split("\\|");
+			if (hm.containsKey(nextValue[0])) {
+				hm.get(nextValue[0]).incrementSum(Float.valueOf(nextValue[1]));
+			} else {
+				hm.put(nextValue[0], new SumCount(Float.valueOf(nextValue[1])));
+				compositeValues += nextValue[0] + " ";
+			}
+		}
+		return compositeValues;
+	}
+	
+	/**
+	 * Gets the name of the values and does the average along 
+	 * the ones with the same name using the Hashmap constructed before.
+	 * The string "x|2.0 x|10.0" will become "x|6.0" after this method is called. 
+	 * @param values
+	 * the string containing the names of the values to transform.
+	 */
+	private String averageWeight(String values, HashMap<String, SumCount> hm) {
+		String newAttributes = "";
+		String[] valuesArray = values.split("\\s");
+		String name = "";
+		float weight = 0;
+		for (int i = 0; i < valuesArray.length; i++) {
+			name = valuesArray[i];
+			weight = hm.get(name).getAverage();
+			newAttributes += name+"|"+weight + " ";
+		}
+		return newAttributes.trim();
+	}
+	
+	/**
+	 * This class stores the total sum and the number of repetition of one
+	 * attribute. So if in importedAttributes there are "x|1.0 x|0.5 y|2.0 x|3.0",
+	 * for x it will store sum = 4.5 and count = 3
+	 */
+	class SumCount {
+		private float sum = 0;
+		private int count = 0;
+		
+		public SumCount(float sum) {
+			this.sum = sum;
+			this.count++;
+		}
+		
+		public void incrementSum(float increment) {
+			this.sum += increment;
+			this.count++;
+		}
+		
+		public float getAverage() {
+			return this.sum / this.count;
+		}
+		
+	}
+	
 }
