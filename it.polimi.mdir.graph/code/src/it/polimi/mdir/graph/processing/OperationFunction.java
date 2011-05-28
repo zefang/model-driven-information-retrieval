89f6bb4454434687045dcfaf39793086e9c41876
Lorenzo.Furrer <Lorenzo.Furrer@dd02c6bc-fb39-5e23-e1a6-f891a138d0db>
2011-05-28 12:30:39 +0000

    - Solved definitively the problem of the different number of terms. - Now I pass the followedEdge directly to importAttributes().

--- it.polimi.mdir.graph/code/src/it/polimi/mdir/graph/processing/OperationFunction.java
+++ it.polimi.mdir.graph/code/src/it/polimi/mdir/graph/processing/OperationFunction.java
@@ -6,7 +6,7 @@ import edu.uci.ics.jung.graph.Graph;
 
 public abstract class OperationFunction {
 
-	abstract public void importAttributes(Node currentNode, Node callerNode, int numHops, Graph<Node, Edge> g);
+	abstract public void importAttributes(Node currentNode, Node callerNode, Edge followedEdge, int numHops, Graph<Node, Edge> g);
 	
 }
 
