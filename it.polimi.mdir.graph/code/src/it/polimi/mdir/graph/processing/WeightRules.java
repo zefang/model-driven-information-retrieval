65bc4a089a0432676a7a4ca998372a5ef172d2c9
stefano.celentano87 <stefano.celentano87@gmail.com>
2011-05-31 10:33:07 +0000

    Updated payloads into WeightRules.java to start test on the system.

--- it.polimi.mdir.graph/code/src/it/polimi/mdir/graph/processing/WeightRules.java
+++ it.polimi.mdir.graph/code/src/it/polimi/mdir/graph/processing/WeightRules.java
@@ -11,28 +11,28 @@ public class WeightRules {
 	{
 		//weights for imported attributes
 		weightMap.put("attribute", 1.0f); //vanilla attribute
-		weightMap.put("composition_1-1", 2.0f);
-		weightMap.put("composition_1-*", 1.5f);
-		weightMap.put("association_1-1", 2.5f);
-		weightMap.put("association_1-*", 2.0f);
-		weightMap.put("class", 5.0f);
+		weightMap.put("composition_1-1", 1.5f);
+		weightMap.put("composition_1-*", 1.3f);
+		weightMap.put("association_1-1", 1.6f);
+		weightMap.put("association_1-*", 1.3f);
+		weightMap.put("class", 2.5f);
 	}
 	
 	public final static HashMap<String, Float> penaltyMap = new HashMap<String, Float>();
 	static
 	{
 		//penalties for relation types during navigation
-		penaltyMap.put(RelationType.COMPOSITION_COMPOSITE_COMPONENT_1_1.toString(), 0.8f);
-		penaltyMap.put(RelationType.COMPOSITION_COMPOSITE_COMPONENT_1_N.toString(), 0.7f);
+		penaltyMap.put(RelationType.COMPOSITION_COMPOSITE_COMPONENT_1_1.toString(), 0.6f);
+		penaltyMap.put(RelationType.COMPOSITION_COMPOSITE_COMPONENT_1_N.toString(), 0.5f);
 		
 		penaltyMap.put(RelationType.COMPOSITION_COMPONENT_COMPOSITE_1_1.toString(), 0.6f);
 		penaltyMap.put(RelationType.COMPOSITION_COMPONENT_COMPOSITE_1_N.toString(), 0.5f);
 
-		penaltyMap.put(RelationType.ASSOCIATION_1_1.toString(), 0.4f);
-		penaltyMap.put(RelationType.ASSOCIATION_1_N.toString(), 0.3f);
+		penaltyMap.put(RelationType.ASSOCIATION_1_1.toString(), 0.6f);
+		penaltyMap.put(RelationType.ASSOCIATION_1_N.toString(), 0.5f);
 
-		penaltyMap.put(RelationType.GENERALIZATION_FATHER_CHILD.toString(), 0.5f);
-		penaltyMap.put(RelationType.GENERALIZATION_CHILD_FATHER.toString(), 0.75f);
+		penaltyMap.put(RelationType.GENERALIZATION_FATHER_CHILD.toString(), 0.75f);
+		penaltyMap.put(RelationType.GENERALIZATION_CHILD_FATHER.toString(), 1.0f);
 	}
 	
 }
