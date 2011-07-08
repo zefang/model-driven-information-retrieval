27141652cffcb6c36c20d4b80f058fe305f5cadb
Lorenzo.Furrer <Lorenzo.Furrer@dd02c6bc-fb39-5e23-e1a6-f891a138d0db>
2011-07-08 11:53:36 +0000

    Added it.polimi.mdir.webml containing first version of the TranslateWebMLToXMI

---
+++ it.polimi.mdir.webml/code/src/it/polimi/mdir/webml/ConfigLoader.java
@@ -0,0 +1,30 @@
+package it.polimi.mdir.webml;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigLoader {
+
+	public static String WEBML_PATH = "";
+	public static String OUTPUT_PATH = "";
+	
+	private static Properties config;
+	static {
+		try {
+			config = new Properties();
+			//config.load(ConfigLoader.class.getClassLoader().getResourceAsStream("configuration.properties"));
+			FileInputStream in = new FileInputStream("C:/Users/Lox/workspaceSMILA/it.polimi.mdir.webml/configuration.properties");
+			config.load(in);
+			
+			WEBML_PATH = config.getProperty("WEBML_PATH");
+			OUTPUT_PATH = config.getProperty("OUTPUT_PATH");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
