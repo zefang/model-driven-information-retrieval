916327dc3366abc307fc63f77dd3b7f414d62479
Lorenzo.Furrer <Lorenzo.Furrer@dd02c6bc-fb39-5e23-e1a6-f891a138d0db>
2011-07-25 10:12:59 +0000

    KeywordExtractorPipelet.java now appends the extracted keywords to C:/keywords.txt (Disabled by default)

--- it.polimi.mdir.webml/code/src/it/polimi/mdir/webml/pipelet/search/KeywordExtractorPipelet.java
+++ it.polimi.mdir.webml/code/src/it/polimi/mdir/webml/pipelet/search/KeywordExtractorPipelet.java
@@ -4,8 +4,12 @@ import it.polimi.mdir.logger.Log;
 import it.polimi.mdir.logger.LogFactory;
 
 import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.util.Iterator;
 
 import org.eclipse.smila.blackboard.Blackboard;
@@ -23,6 +27,8 @@ import org.jdom.input.SAXBuilder;
 /**
  * This pipelet extracts the keywords to submit as a text query 
  * from the webml projects saved in XMI format
+ * 
+ * It also appends to a txt file the extracted keywords. 
  *
  */
 public class KeywordExtractorPipelet implements Pipelet {
@@ -61,6 +67,15 @@ public class KeywordExtractorPipelet implements Pipelet {
 			
 			System.out.println("keywordString = " + keywordString);
 			
+			//Append keywords to file
+			//Disabled by default
+			/*
+			File f = new File("C:/keywords.txt");
+			FileWriter fw = new FileWriter(f, true);
+			PrintWriter pw = new PrintWriter(fw);
+			pw.println(keywordString);
+			pw.close();
+			*/
 		} catch (Exception e) {
 			_log.write("Error in KeywordExtractorPipelet: " + e.toString());
 			e.printStackTrace();
@@ -72,9 +87,9 @@ public class KeywordExtractorPipelet implements Pipelet {
 	
 	/**
 	 * Checks if the elements is eligible to have the name extracted as a keyword.
-	 * TODO: Discard SiteView and Areas that contain only other areas 
-	 *       in order to be comparable with the Graph matching algorithm.
-	 *       In general in the query case it would be better to keep them
+	 * Discard SiteView and Areas that contain only other areas 
+	 * in order to be comparable with the Graph matching algorithm.
+	 * In general in the query case it would be better to keep them
 	 */
 	private boolean checkEligibility(Element element) {
 		if (element.getAttributeValue("type", XMI_NAMESPACE) == null) {
@@ -104,6 +119,21 @@ public class KeywordExtractorPipelet implements Pipelet {
 		if (element.getAttributeValue("type", XMI_NAMESPACE).equals("webml:KOLink")) {
 			return false;
 		}
+		
+		if (element.getAttributeValue("type", XMI_NAMESPACE).equals("webml:SiteView") 
+			|| element.getAttributeValue("type", XMI_NAMESPACE).equals("webml:Area")) {
+			//check if they have just subareas (e.g. if their children are only Areas)
+			//if there are only Areas as children then it is not eligible
+			boolean justAreas = true;
+			Iterator<Element> childrenItr = element.getChildren().iterator();  
+			while (childrenItr.hasNext()) {
+				if (!childrenItr.next().getAttributeValue("type", XMI_NAMESPACE).equals("webml:Area")) {
+					justAreas = false;
+				}
+			}
+			return !justAreas;
+		}
+		
 		return true;
 	}
 
